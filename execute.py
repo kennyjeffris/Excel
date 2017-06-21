0f6283a95e626bc44a0175d41c4edb02600e96b9
Kenny Jeffris <kenny.jeffris@bio-techne.com>
2017-06-21 14:29:32 -0700

    Created .gitignore

--- execute.py
+++ execute.py
@@ -14,24 +14,27 @@ f = open(filename)
 # Change this when implementing into program
 ##############################################
 
-
-
+# Set up CSV tools
 csv.register_dialect('comma', delimiter=',')
-
 reader = csv.reader(f, dialect='comma')
 
+# Initialize .xlsx file
 wb = Workbook()
 (new, extra) = split(filename)
-newName = 'blah.xlsx'
+newName = 'output.xlsx'
 print(new)
 dest_filename = join(new, newName)
 
+# Create first sheet
 ws1 = wb.worksheets[0]
 ws1.title = "Raw Data"
 
+# Copy contents of original output to new sheet
 for row_index, row in enumerate(reader):
     for column_index, cell in enumerate(row):
         column_letter = get_column_letter((column_index + 1))
         ws1.cell('%s%s'%(column_letter, (row_index + 1))).value = cell
+
+ws2 = wb.create_sheet(title='')
 
 wb.save(filename = dest_filename)
