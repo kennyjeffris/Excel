9d326bf5af6419ae77a2a3f89561a53a85ef4a39
Kenny Jeffris <kenny.jeffris@bio-techne.com>
2017-08-01 18:49:00 -0700

    Renamed, added features

--- setup.py
+++ setup.py
@@ -12,20 +12,22 @@ Include_Modules = [
 
 Exclude_Modules = [
     "ipykernel", "iPython", "ipython_genutils", "jedi", "matplotlib", "pandas", "scipy", "lib2to3", "jupyter_client",
-    "jupyter_core", "tcl8.6/tzdata", "nose", "unittest", "collections"
+    "jupyter_core", "tcl8.6/tzdata", "nose", "unittest"
 ]
 
 buildOptions = {"includes": Include_Modules, "include_files": ["tcl86t.dll", "tk86t.dll", "proteinsimple_logo_bt.ico"],
-                "excludes": Exclude_Modules}
+                "excludes": Exclude_Modules,
+                "optimize": 2
+                }
 
 base = 'Win32GUI' if sys.platform == 'win32' else None
 
 executables = [
-    Executable('formatCSV.py', base=base, icon="proteinsimple_logo_bt.ico")
+    Executable('SimplePlexAnalyzer.py', base=base, icon="proteinsimple_logo_bt.ico")
 ]
 
 setup(name='Simple Plex Analyzer',
-      version='1.0',
+      version='1.0.1',
       description='Data organizer for Simple Plex output.',
       options=dict(build_exe=buildOptions),
       executables=executables)
