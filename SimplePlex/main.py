33cedc27b72953252e8565c0753eea3162e637ac
kjeffrisps <kenny.jeffris@bio-techne.com>
2017-08-11 11:18:34 -0700

    Updated get_analytes, made SimplePlex source root for recognizing files

--- SimplePlex/main.py
+++ SimplePlex/main.py
@@ -3,26 +3,27 @@ from tkinter import messagebox
 import sys
 import csv
 import tkinter as tk
+import string
 from openpyxl import Workbook
-from openpyxl.compat import range
 from openpyxl.utils import get_column_letter
-from openpyxl.styles import Alignment, PatternFill, colors
-from openpyxl.styles.borders import Border, Side
 
 ##############################
 # Global variables
 root = tk.Tk()
 max_row = 0
 max_col = 0
+num_samples = 0
 ##############################
+
 
 def main():
     file = get_file()
     wb = Workbook()
-    wb = init_raw_data(wb)
+    wb = init_raw_data(file, wb)
     analytes = get_analytes(wb)
     wb = format_file(wb, analytes)
     save_file(file, wb)
+
 
 def get_file():
     global root
@@ -48,6 +49,7 @@ def get_file():
     file = open(filename)
     return file
 
+
 def save_file(file, wb):
     newName = 'output.xlsx'
 
@@ -63,10 +65,13 @@ def save_file(file, wb):
     if not dest_filename:
         sys.exit()
 
-def init_raw_data(wb, reader):
+
+def init_raw_data(file, wb):
     # Create first sheet
     global max_col
     global max_row
+    csv.register_dialect('comma', delimiter=',')
+    reader = csv.reader(file, dialect='comma')
     ws1 = wb.worksheets[0]
     ws1.title = 'Raw data'
     for row_index, row in enumerate(reader):
@@ -75,30 +80,31 @@ def init_raw_data(wb, reader):
             ws1['%s%s' % (column_letter, (row_index + 1))].value = cell
             max_col = column_index + 1
         max_row = row_index + 1
+    return wb
+
 
 def get_analytes(wb):
     try:
+        ws1 = wb.worksheets[0]
         analyte_order = []
         done = False
         count = 1
         while not done:
             letter = get_column_letter(count)
             ind = letter + '1'
-            if wb.ws1[ind].value == "AnalyteName":
-                '''for l in range(2, 6):
-                    ind = letter + str(l)
-                    try:
-                        analyteOrder.append(str(wb.ws1[ind].value))
-                    except Exception:
-                        analyteOrder.append(wb.ws1[ind].value)
-                break'''
-
-                # REWRITE SMARTER CODE HERE FIXME
-
-                if not analyte_order:
-                    analyte_order.append(str(wb.ws1[ind].value))
-                else:
-                    pass #FIXME
+            if ws1[ind].value == "AnalyteName":
+                count = 2
+                while not done:
+                    ind = letter + str(count)
+                    if not analyte_order:
+                        analyte_order.append(str(ws1[ind].value))
+                    else:
+                        obj = str(ws1[ind].value)
+                        if not obj in analyte_order:
+                            analyte_order.append(obj)
+                        else:
+                            done = True
+                    count += 1
             count += 1
             if count == max_col:
                 raise ValueError('Item not found')
@@ -106,18 +112,33 @@ def get_analytes(wb):
         messagebox.showerror(message="Missing Analyte Names.  Please export your data with "
                                      "this item included", title="Failure")
         sys.exit()
+    global num_samples
+    num_samples = get_num_samples(ws1, analyte_order)
     return analyte_order
+
 
 def format_file(wb, analytes):
     if len(analytes) == 1:
         from one_by_72 import format
     else:
-        num_samples = get_num_samples(wb, analytes)
         if num_samples == 16:
             from four_by_16 import format
         elif num_samples == 36:
             from four_by_36 import format
     return format(wb, analytes)
+
+
+def col2num(colindex):
+    """Convert excel column letter to index number."""
+    number = 0
+    for c in colindex:
+        if c in string.ascii_letters:
+            number = number * 26 + (ord(c.upper()) - ord('A')) + 1
+    return number
+
+
+def get_num_samples(ws, analytes):
+    return int((max_row - 1) / len(analytes))
 
 if __name__ == '__main__':
     sys.exit(main())
