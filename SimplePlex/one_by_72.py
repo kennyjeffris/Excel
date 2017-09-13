494837edd42fb0de3b6e15f5c28810cb007be3a7
kjeffrisps <kenny.jeffris@bio-techne.com>
2017-09-12 19:07:02 -0700

    Pre-Release

--- SimplePlex/one_by_72.py
+++ SimplePlex/one_by_72.py
@@ -11,7 +11,7 @@ def format(wb, analytes, max_row, max_col):
     style = styles.get()
     headerlist, searchlist = prep_lists(analytes)
     wb = summaries_1_2(wb, analytes, headerlist, searchlist, max_row, max_col)
-    #wb = summary_3(wb, analytes, headerlist, searchlist, max_row, max_col)
+    wb = summary_3(wb, analytes, headerlist, searchlist, max_row, max_col)
     return wb
 
 
@@ -88,20 +88,21 @@ def summaries_1_2(wb, analytes, headerList, searchList, max_row, max_col):
             cell.value = 'Calculated Concentration'
             cell.alignment = style['center_center']
             cell.fill = style['yellow_fill']
-        for index, col in enumerate(iterable=workingSheet.iter_cols(min_row=2, max_row=2,
-                                                                    min_col=3, max_col=17)):
-            for cell in col:
-                cell.border = style['medium_thin']
-        else:
             for index, col in enumerate(iterable=workingSheet.iter_cols(min_row=2, max_row=2,
-                                                                        min_col=3, max_col=7)):
+                                                                        min_col=3, max_col=17)):
                 for cell in col:
                     cell.border = style['medium_thin']
+        else:
+
             workingSheet.merge_cells(start_row=2, start_column=3, end_row=2, end_column=7)
             cell = workingSheet['C{}'.format(2)]
             cell.value = 'Calculated Concentration'
             cell.alignment = style['center_center']
             cell.fill = style['yellow_fill']
+            for index, col in enumerate(iterable=workingSheet.iter_cols(min_row=2, max_row=2,
+                                                                        min_col=3, max_col=7)):
+                for cell in col:
+                    cell.border = style['medium_thin']
         for index, col in enumerate(iterable=workingSheet.iter_cols(
                 min_row=3,
                 min_col=1, max_row=3,
@@ -136,5 +137,146 @@ def summaries_1_2(wb, analytes, headerList, searchList, max_row, max_col):
                     cell.border = style['thin']
     return wb
 
-def summary_3(wb, analytes, headerlist, searchlist, max_row, max_col):
+def summary_3(wb, analytes, headerLists, searchLists, max_row, max_col):
+    ws4 = wb.create_sheet(title='Summary 3')
+    ws1 = wb.worksheets[0]
+    headerList3 = headerLists[2]
+    headerList4 = headerLists[3]
+    headerList5 = headerLists[4]
+    ws4['A1'].value = headerList4[0]
+    for index, col in enumerate(iterable=ws4.iter_cols(
+            min_row=2,
+            min_col=1, max_row=2,
+            max_col=len(headerList3))):
+        for cell in col:
+            cell.value = headerList3[index]
+            cell.border = style['medium']
+
+    for index, row in enumerate(iterable=ws4.iter_rows(
+            min_row=3,
+            max_col=1, max_row=74)):
+        for cell in row:
+            cell.value = index + 1
+            cell.border = style['medium_thin']
+
+    for index, col in enumerate(iterable=ws4.iter_cols(
+            min_col=2, max_col=len(headerList3),
+            min_row=3, max_row=3)):
+
+        values = get_items(ws1, headerList4[0], max_row, max_col)
+        for index2, row in enumerate(iterable=ws4.iter_rows(
+                min_col=index + 2,
+                max_col=index + 2,
+                min_row=3,
+                max_row=74)):
+            for cell in row:
+                cell.value = values[index2]
+                if cell.value == 'ND':
+                    cell.fill = style['red_fill']
+                cell.alignment = style['right_center']
+                cell.border = style['thin']
+
+    ws4['A76'].value = headerList5[0]
+    ws4['A77'].border = style['medium']
+    for index, col in enumerate(iterable=ws4.iter_cols(min_row=77,
+                                                       min_col=2, max_row=77, max_col=2)):
+        for cell in col:
+            cell.value = headerList3[index + 1]
+            cell.border = style['medium']
+
+    for index, row in enumerate(iterable=ws4.iter_rows(min_row=78, max_row=82,
+                                                       min_col=1, max_col=1)):
+        for cell in row:
+            cell.value = headerList5[index + 1]
+            cell.border = style['medium_thin']
+
+    for index, col in enumerate(iterable=ws4.iter_cols(
+            min_col=2, max_col=len(headerList3),
+            min_row=78, max_row=82)):
+
+        for index2, row in enumerate(iterable=ws4.iter_rows(
+                min_col=index + 2,
+                min_row=78,
+                max_row=82)):
+            values = get_items(ws1, headerList4[index2 + 1], max_row, max_col)
+            for cell in row:
+                cell.value = values[0]
+                cell.border = style['thin']
+
+    ##############################################
+    # Add plots to Summary 3
+
+    for index, col in enumerate(iterable=ws4.iter_cols(
+            min_col=2,
+            max_col=2)):
+        coeffs = []
+        let = get_column_letter(2 + index)
+        for index2, row in enumerate(iterable=ws4.iter_rows(
+                range_string='{}78:{}82'.format(let,
+                                                let))):
+            for cell in row:
+                coeffs.append(cell.value)
+        xvalues = []
+        values = []
+        xvalues.extend((.1, 1, 10, 100, 1000, 10000))
+        for num in range(0, len(xvalues)):
+            if isinstance(xvalues[num], (int, float, complex)):
+                values.append(poly_fit(xvalues[num], coeffs))
+        for index3, row in enumerate(iterable=ws4.iter_rows(
+                min_row=3,
+                max_row=74,
+                min_col=2,
+                max_col=2)):
+            for cell in row:
+                xval = cell.value
+                if isinstance(xval, (int, float, complex)):
+                    xvalues.append(xval)
+                    values.append(poly_fit(xval, coeffs))
+
+        xvalues, values = zip(*sorted(zip(xvalues, values)))
+        xvalues, values = (list(t) for t in zip(*sorted(zip(xvalues, values))))
+        count1 = 0
+        count2 = 0
+        for index4, row in enumerate(iterable=ws4.iter_rows(
+                range_string='{}84:{}240'.format(let, let))):
+            for cell in row:
+                if count1 < len(xvalues):
+                    cell.value = xvalues[count1]
+                    count1 += 1
+                elif count1 >= len(values) and count2 < len(values):
+                    cell.value = values[count2]
+                    count2 += 1
+                else:
+                    break
+        xref = Reference(ws4, min_col=2 + index, max_col=2 + index,
+                         min_row=84, max_row=83 + len(xvalues))
+        yref = Reference(ws4, min_col=2 + index, max_col=2 + index,
+                         min_row=84 + len(xvalues),
+                         max_row=83 + len(xvalues) + len(values))
+
+        # Chart formatting
+        chart = ScatterChart()
+        # Title
+        chart.title = '{}'.format(headerList3[index + 1])
+        chart.style = 13
+        chart.legend = None
+        chart.x_axis.scaling.logBase = 10
+        chart.y_axis.scaling.logBase = 10
+        chart.x_axis.scaling.min = 0.1
+        chart.y_axis.scaling.min = 0.1
+        chart.x_axis.scaling.max = 10000
+        chart.y_axis.scaling.max = 10000
+        chart.y_axis.crossesAt = 0.1
+        chart.y_axis.title = 'RFU'
+        chart.x_axis.title = 'Concentration (pg/ml)'
+        chart.x_axis.tickLblPos = "low"
+        series = Series(yref, xref, title_from_data=False)
+        series.marker = marker.Marker('circle')
+        series.smooth = True
+        series.graphicalProperties.line.width = 20000
+        series.graphicalProperties.line.solidFill = ColorChoice(prstClr="orange")
+        series.marker.graphicalProperties.solidFill = ColorChoice(prstClr="orange")  # Marker filling
+        series.marker.graphicalProperties.line.solidFill = ColorChoice(prstClr="orange")  # Marker outline
+        chart.series.append(series)
+        ws4.add_chart(chart, 'D{}'.format((index * 15) + 1))
     return wb
